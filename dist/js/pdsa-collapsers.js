11e94b34889364b99ea55e5a132edac245ff77fa
ajdin <ajdika@live.com>
2016-11-07 12:32:01 +0000

    (1) Let's go after anything that has this attribute, data-pdsa-collapser-name, give me all of those things as a collection; (2) Then I'll loop through the pdsaCollapsers.length (3) Now all the remaining code below needs to go into the for loop; (4) Now let's get the name of the pdsaCollapser container, and get at its id, and put this id into var 'name'; (5) Now we have vars - name, open, close, but we need to replace the reference to #playlists, that's hardcoded into both open and close var assignment, and we'll replace it with ('#' + name)

--- dist/js/pdsa-collapsers.js
+++ dist/js/pdsa-collapsers.js
@@ -3,13 +3,15 @@ var pdsaCollapsers = $("[data-pdsa-collapser-name]"); // (1)
 
 for (var i = 0; i < pdsaCollapsers.length; i++) { // (2)
     
-    var open = $("#playlists").data("pdsa-collapser-open");
-    var close = $("#playlists").data("pdsa-collapser-close");
+    var name = pdsaCollapsers[i].id;
     
-      $("#playlists .pdsa-panel-toggle")
+    var open = $("#" + name).data("pdsa-collapser-open");
+    var close = $("#" + name).data("pdsa-collapser-close");
+    
+      $("#" + name " .pdsa-panel-toggle")
       .addClass(open);
       
-      var list = $("#playlists .in");
+      var list = $("#" + name " .in");
       for (var i=0; i < list.length; i++) {
       	$("a[href='#" + $(list[i]).attr("id") + "']") 
       	.next(".pdsa-panel-toggle")
@@ -17,21 +19,23 @@ for (var i = 0; i < pdsaCollapsers.length; i++) { // (2)
       	.addClass(close);
       }
     
-      $("#playlists").on('hide.bs.collapse', function(e) {
+      $("#" + name).on('hide.bs.collapse', function(e) {
       	$("#" + e.target.id).prev()
       	.find(".pdsa-panel-toggle")
-      	.removeClass($("#playlists").data("pdsa-collapser-close"))
-      	.addClass($("#playlists").data("pdsa-collapser-open"));
+      	.removeClass($("#" + name).data("pdsa-collapser-close"))
+      	.addClass($("#" + name).data("pdsa-collapser-open"));
       });
     
-      $("#playlists").on('show.bs.collapse', function(e) {
+      $("#" + name).on('show.bs.collapse', function(e) {
       	$("#" + e.target.id).prev()
       	.find(".pdsa-panel-toggle")
-      	.removeClass($("#playlists").data("pdsa-collapser-open"))
-      	.addClass($("#playlists").data("pdsa-collapser-close"));
+      	.removeClass($("#" + name).data("pdsa-collapser-open"))
+      	.addClass($("#" + name).data("pdsa-collapser-close"));
       });
     });
 }
 // (1) Let's go after anything that has this attribute, data-pdsa-collapser-name, give me all of those things as a collection
 // (2) Then I'll loop through the pdsaCollapsers.length
 // (3) Now all the remaining code below needs to go into the for loop
+// (4) Now let's get the name of the pdsaCollapser container, and get at its id, and put this id into var 'name'
+// (5) Now we have vars - name, open, close, but we need to replace the reference to #playlists, that's hardcoded into both open and close var assignment, and we'll replace it with ('#' + name)
